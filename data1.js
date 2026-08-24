afa75b8de4fef82d1816dfa7742604f790a527d8
pedrosergiojacobrosa-dot <pedrosergiojacobrosa@gmail.com>
2026-08-24 19:40:08 -0300

    Carrega configuração do Supabase antes do app

---
+++ data1.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  supabaseUrl: 'https://rtnigoszuttbmyvzgscu.supabase.co',
+  supabaseKey: 'sb_publishable_tnlE3wVwIT8RHkPXJb5-jQ_MxlwhnKR'
+};
+window.IGREJAS = window.IGREJAS || [];
