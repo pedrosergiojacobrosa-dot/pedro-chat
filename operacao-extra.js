9939abcd8c962a24193b6bb309761f93c3656f26
pedrosergiojacobrosa-dot <pedrosergiojacobrosa@gmail.com>
2026-09-05 18:03:06 -0300

    Carrega rota alternativa de Sorocaba

--- operacao-extra.js
+++ operacao-extra.js
@@ -33,4 +33,5 @@ setTimeout(()=>{if(authUser)carregarIgrejaCfg();},1200);
 (function(){const s=document.createElement('script');s.src='rotas-flex-v7.js?v=1';s.async=false;document.body.appendChild(s);})();
 (function(){const s=document.createElement('script');s.src='rota-cards-flex-v1.js?v=1';s.async=false;document.body.appendChild(s);})();
 (function(){const s=document.createElement('script');s.src='avisos-operacionais-v1.js?v=1';s.async=false;document.body.appendChild(s);})();
-(function(){const s=document.createElement('script');s.src='visitas-rotas-v1.js?v=1';s.async=false;document.body.appendChild(s);})();
+(function(){const s=document.createElement('script');s.src='visitas-rotas-v1.js?v=2';s.async=false;document.body.appendChild(s);})();
+(function(){const s=document.createElement('script');s.src='rota-alternativa-sorocaba-v1.js?v=1';s.async=false;document.body.appendChild(s);})();
